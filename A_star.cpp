ae14030d663986e5820fc3a567770acb53fb0497
amacp001 <amacp001@ucr.edu>
2017-02-07 22:56:13 -0800

    Completed the input count error checking for the custom puzzle maker

--- A_star.cpp
+++ A_star.cpp
@@ -1,19 +1,67 @@
 #include <iostream>
+#include <string.h>
 #include <math.h>
 #include "Problem.h"
 
 int puzzle[TILE_CNT] = {8, 7, 6, 5, 4, 3, 2, 1, 0};
 int puzzle_dimension = (int)sqrt((int)TILE_CNT);
 
-void custom_puzzle_maker()
+bool isInteger(char *num)
 {
-		printf("Enter your puzzle, use a zero to represent the blank\n");
-		for(int i = 0; i < puzzle_dimension; i++)
+	return false;
+}
+
+void customPuzzleMaker()
+{
+	std::string input;
+	printf("Enter your puzzle, use a zero to represent the blank\n");
+	int i = 0;
+	bool valid_num;
+	while(i < puzzle_dimension)
+	{
+		printf("Enter %d numbers for row %d, use space or tabs between numbers:\t", puzzle_dimension, i+1);
+		std::getline(std::cin, input);
+		char *input_numbers = new char[input.length()+1];
+		std::strcpy(input_numbers, input.c_str());
+		char *number = std::strtok(input_numbers, " \t");
+		//Check to see if enough numbers were entered and if the correct numbers were entered
+		valid_num = true;
+		int num_cnt = 0;
+		while(number != 0)
 		{
-			printf("Enter row %d, use space or tabs between numbers:\t", i+1);
-			//Errors: letters/special characters, negative numbers, too large numbers
-			//Errors: duplicate numbers in the array
+			//If not a number, error
+			if(isInteger(number))
+			{
+				valid_num = false;
+				printf("ERROR: Number %s is not a number\n", number);
+			}
+			//If not a valid number, error
+			else if(0)
+			{
+				valid_num = false;
+				printf("ERROR: Number %d is negative\n", 1);
+				printf("ERROR: Number %d is too big\n", 1); 
+			}
+			number = std::strtok(NULL, " \t");
+			num_cnt++;
 		}
+		if(!valid_num)
+		{
+			printf("       Valid inputs are whole numbers from 0 - %d\n", TILE_CNT-1);
+			delete[] input_numbers;
+			continue;
+		}
+		//Check for correct number of numbers
+		if(num_cnt != puzzle_dimension)
+		{
+			printf("ERROR: %d number(s) were entered for row %d\n", num_cnt, i+1);
+			delete[] input_numbers;
+			continue;
+		}
+		//Errors: duplicate numbers in the array
+		delete[] input_numbers;
+		i++;
+	}
 }
 
 int main(int argc, char **argv)
@@ -22,6 +70,8 @@ int main(int argc, char **argv)
 	printf("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle\n");
 	std::string choice;
 	std::cin >> choice;
+	std::cin.clear();
+	std::cin.ignore(100, '\n');
 	for(; choice != "1" && choice != "2"; std::cin >> choice)
 	{
 		printf("Invalid Response!\n");
@@ -29,7 +79,7 @@ int main(int argc, char **argv)
 	}
 	if(choice == "2")
 	{
-		custom_puzzle_maker();
+		customPuzzleMaker();
 	}
 	return 0;
 }
